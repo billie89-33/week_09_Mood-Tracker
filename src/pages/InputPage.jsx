7aec542173a9676757a2a762abb126b502926e2a
Wathisa <wathisa@gmail.com>
2026-04-28 13:28:36 +0700

    add Input and Display page

--- src/pages/InputPage.jsx
+++ src/pages/InputPage.jsx
@@ -8,6 +8,31 @@ const InputPage = () => {
       <p className="text-gray-500 mt-2">เขียนโค้ดบันทึกอารมณ์ของคุณตรงนี้...</p>
       
       {/* เริ่มเขียนโค้ดของคุณที่นี่ */}
+      <div className="input-page">
+        <div className="input-title">
+          <h2>What's your mood?</h2>
+        </div>
+
+        <div className="input-emoji">
+          <div className="emoji-component">Emoji</div>
+        </div>
+
+        <div className="input-date">
+          <div className="add-date-component">Add-date</div>
+        </div>
+
+        <div className="input-mood">
+          <div className="add-mood-component">Add-mood</div>
+        </div>
+
+        <div className="input-buttons">
+          <div className="mood-buttons-component">Mood-buttons</div>
+        </div>
+
+        <div className="input-save">
+          <div className="save-component">Save</div>
+        </div>
+      </div>
       
     </div>
   );
